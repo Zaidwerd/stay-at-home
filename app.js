f2734b72c8078ac96e27a2c3d5d2cfd6725eb759
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-10-26 13:57:34 -0400

    set up MVC and server

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const homeRoute = require('./routes/index');
+const generatorRoute = require('./routes/generator');
+const savedRoute = require('./routes/saved');
+
+const app = express();
+const port = process.argv[2] || process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.use('/', homeRoute);
+app.use('/generator', generatorRoute);
+app.use('/saved', savedRoute);
+
+app.listen(port, () => console.log('listening on port ', port));
