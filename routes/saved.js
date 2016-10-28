563ab169d538424d3787fcdfb28d76be4354fa60
Andrew Diaz <andrew.matt.diaz@gmail.com>
2016-10-28 10:50:17 -0400

    add css and move search form to generator.ejs

--- routes/saved.js
+++ routes/saved.js
@@ -1,14 +1,18 @@
 const router = require('express').Router();
 const dbService = require('../models/savedDB');
 
-router.post('/', dbService.addSaved, dbService.getSaved, (req, res) => {
+router.get('/', dbService.getSaved, (req, res) => {
   res.render('saved', {
     favorites: res.saved,
   });
 });
 
+router.post('/', dbService.addSaved, (req, res) => {
+  res.redirect('saved');
+});
+
 router.delete('/saved/:id', dbService.deleteSaved, (req, res) => {
-  res.redirect('/saved');
+  res.redirect('saved');
 });
 
 module.exports = router;
